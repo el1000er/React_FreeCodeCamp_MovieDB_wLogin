88511f40e3117574a0c3ff6759a0bb2c259ff6aa
Agustin Miller Araújo <agusmill@hotmail.com>
2021-09-11 18:19:58 -0300

    Getting movies from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+
+//Styles
+import {GlobalStyle} from './GlobalStyle';
+
+//without jsx Example
+// const Star =()=> React.createElement('div', null,'This is a litte Star');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // //without jsx
+  // return  Star();
+  return(<div className="App">
+    <Header/>    
+    <Home/>
+    <GlobalStyle/>
+  </div>
+  )
+ 
 }
 
 export default App;
