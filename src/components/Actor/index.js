c053b637c876a62c2a9461c50f6fd6d4e3cc6249
Agustin Miller Araújo <agusmill@hotmail.com>
2021-09-12 20:40:02 -0300

    Added Actor styles properties

--- src/components/Actor/index.js
+++ src/components/Actor/index.js
@@ -6,7 +6,7 @@ const Actor=({name,character,imageUrl})=>(
     <Wrapper>
         <Image src={imageUrl} alt='actor-thumb'/>
         <h3>{name}</h3>
-        <p>{character}</p>
+        <p>{character}</p>         
     </Wrapper>
 );
 
